9ad684986fb6cf7befeaaf3b9cbf766c1ce999a1
gmdiniz <gabrieldinizmota@hotmail.com>
2020-12-16 19:20:41 -0300

    Indentando codigo

--- public/server/server.php
+++ public/server/server.php
@@ -1,39 +1,39 @@
 <?php
+    include('connection.php');  
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
-if(!isset($_POST)) die();
+    if(!isset($_POST)) {
+        die();
+    }
 
-session_start();
+    session_start();
+    $response = []; 
 
-$response = []; 
+    $input = file_get_contents("php://input");
+    
+    try {
+        $post = json_decode($input, 1);
+    } catch (Exception $e) {
+        $post = $_POST;
+    }
 
-include('connection.php');
+    $_POST = $post;
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $senha = mysqli_real_escape_string($con, $_POST['senha']);
+    $query = "SELECT * FROM `usuario` WHERE email='$email' AND senha='$senha'";
+    $result = mysqli_query($con, $query);
 
-$input = file_get_contents("php://input");
-try {
-    $post = json_decode($input, 1);
-} catch (Exception $e) {
-    $post = $_POST;
-}
-$_POST = $post;
+    if(mysqli_num_rows($result) > 0) {
+        $response['status'] = 'loggedin';
+        $response['user'] = $email;
+        $response['id'] = md5(uniqid());
+        $_SESSION['id'] = $response['id'];
+        $_SESSION['user'] = $email;
+    } else {
+        $response['status'] = 'error';
+    }
 
-$email = mysqli_real_escape_string($con, $_POST['email']);
-$senha = mysqli_real_escape_string($con, $_POST['senha']);
-
-$query = "SELECT * FROM `usuario` WHERE email='$email' AND senha='$senha'";
-
-$result = mysqli_query($con, $query);
-
-if(mysqli_num_rows($result) > 0) {
-	$response['status'] = 'loggedin';
-    $response['user'] = $email;
-    $response['id'] = md5(uniqid());
-	$_SESSION['id'] = $response['id'];
-	$_SESSION['user'] = $email;
-} else {
-	$response['status'] = 'error';
-}
-
-echo json_encode($response);
+    echo json_encode($response);
+?>
